6ed4e980d22ab70f7d288890533ef47d07a99f8a
mehrdad sadafi <mehrdad.sadafi@gmail.com>
2022-12-25 20:05:51 +0330

    added one comment to the first line

--- odd.py
+++ odd.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime  # imports modules
 
 odds = [x for x in range(1, 60) if x % 2 != 0]
 
@@ -8,5 +8,3 @@ if right_this_minute in odds:
     print('this minute seem a little odd.')
 else:
     print('Not an odd minute.')
-
-
