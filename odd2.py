4ce715a668f0badda1fe761dab0476db74e0ae55
mehrdad sadafi <mehrdad.sadafi@gmail.com>
2022-12-25 21:59:55 +0330

    define a for loop with range 5 and completed code by waite_time and time_sleep.

--- odd2.py
+++ odd2.py
@@ -3,3 +3,13 @@ import random
 import time
 
 odds = [x for x in range(1, 60) if x % 2 != 0]
+
+for i in range(5):
+    right_this_minute = datetime.today().minute
+    if right_this_minute in odds:
+        print('this minute seem a little odd.')
+    else:
+        print('Not an odd minute.')
+        wait_time = random.randint(1, 3)
+        time.sleep(wait_time)
+
