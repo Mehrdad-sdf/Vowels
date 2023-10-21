0bdcf1699edc7bb2f4f2947cb12a033cbf46c819
mehrdad sadafi <mehrdad.sadafi@gmail.com>
2023-10-21 15:26:44 +0330

    transposed data and write for loop nested for dispaly dataTable

--- print_Table.py
+++ print_Table.py
@@ -6,4 +6,15 @@ tableData = [['name', 'mehrdad', 'ali', 'javad'],
 def print_rigth_justified(table_data: list) -> str:
     # Find the maximum width for each column
     col_widths = [max(len(item) for item in column) for column in tableData]
-    
+
+    # Transpose the table data to iterate over rows
+    transposed_data = zip(*tableData)
+
+    # print the table
+    for row in transposed_data:
+        for item, width in zip(row, col_widths):
+            print(item.rjust(width), end=' ')
+        print()
+
+
+print_rigth_justified(tableData)
